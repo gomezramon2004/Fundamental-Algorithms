02420958cdc00ad9fa1788ce93face94e79710a4
Ramón Alberto Gómez Urquídez <gomez.ramon2004@gmail.com>
2023-09-20 17:40:00 -0700

    error handling implemented

--- main.cpp
+++ main.cpp
@@ -6,17 +6,23 @@
 #include <vector>
 
 int main() {
-    std::vector<Info> bitacora = parse("bitacora.txt");
-    mergeSort(bitacora, 0, bitacora.size() - 1);
+    try {
+        std::vector<Info> bitacora = parse("bitacora.txt");
+        mergeSort(bitacora, 0, bitacora.size() - 1);
 
-    for (auto &&i : bitacora) {
-        std::cout << "dia: " << i.dateTime.tm_mday << " ";
-        std::cout << "mes: " << i.dateTime.tm_mon << " ";
-        std::cout << "año: " << i.dateTime.tm_year << " ";
-        std::cout << "hora: " << i.dateTime.tm_hour << " ";
-        std::cout << "minutos: " << i.dateTime.tm_min << " ";
-        std::cout << "entrada: " << i.enterPoint << " ";
-        std::cout << "ubi: " << i.ubi << "\n";
+       for (auto &&i : bitacora) {
+            std::cout << "dia: " << i.dateTime.tm_mday << " ";
+            std::cout << "mes: " << i.dateTime.tm_mon << " ";
+            std::cout << "año: " << i.dateTime.tm_year << " ";
+            std::cout << "hora: " << i.dateTime.tm_hour << " ";
+            std::cout << "minutos: " << i.dateTime.tm_min << " ";
+            std::cout << "entrada: " << i.enterPoint << " ";
+            std::cout << "ubi: " << i.ubi << "\n";
+        }
+
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
     }
 
     return 0;
