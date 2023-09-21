3e3fe7c955d77db0aa3fb63a1da3613a375fa6ff
Ramón Alberto Gómez Urquídez <gomez.ramon2004@gmail.com>
2023-09-20 21:38:31 -0700

    optimized parse functionality

--- parse/parse.cpp
+++ parse/parse.cpp
@@ -1,35 +1,39 @@
 #include "parse.hpp"
-#include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
 #include <ctime>
 #include <algorithm>
-#include <stdexcept> 
+#include <sstream>
+#include <stdexcept>
 
 std::vector<Info> parse(std::string fileName) {
-    std::vector<Info> parsedVec;
-    std::ifstream inputFile;  // RAII is responsible for destruct the file-handling object out of scope. It will handle file closing automatically
-    std::string line;
-    inputFile.open(fileName);
-
+    std::ifstream inputFile(fileName);  // RAII is responsible for destruct the file-handling object out of scope. It will handle file closing automatically
     if (!inputFile.is_open()) {
         throw std::runtime_error("Error - Unable to open the file.");
     }
+    
+    const size_t lineCount = std::count_if(std::istreambuf_iterator<char>(inputFile), std::istreambuf_iterator<char>(), [](char c) { return c == '\n'; });
+    inputFile.seekg(0);
+    std::vector<Info> parsedVec(lineCount);
 
-    while (std::getline(inputFile, line)) {
+    for (size_t i = 0; i < lineCount; ++i) {
+        std::string line;
+        std::getline(inputFile, line);
         const std::string dateTime = line.substr(0, 16);
         struct tm timeStruct;
-
-        sscanf(dateTime.c_str(), "%d/%d/%d %d:%d", &timeStruct.tm_mday, &timeStruct.tm_mon, &timeStruct.tm_year, &timeStruct.tm_hour, &timeStruct.tm_min);
-        timeStruct.tm_mon -= 1;
-
         std::time_t time = mktime(&timeStruct);
-        parsedVec.push_back({time, timeStruct, line[17], line.substr(19, 24)});
+
+        std::istringstream ss(dateTime);
+        ss >> std::get_time(&timeStruct, "%d/%m/%Y %H:%M");
+        timeStruct.tm_mon -= 1;
+        
+        parsedVec[i] = {time, timeStruct, line[17], line.substr(19, 24)};
     }
+
     return parsedVec;
 }
 
 // strptime(dateTime,"%d/%m/%Y %H:%M", &timeStruct); We can't use strptime because Windows isn't POSIX Compliant
-//  https://stackoverflow.com/questions/321849/strptime-equivalent-on-windows
-//  https://www.quora.com/Why-doesnt-Microsoft-make-Windows-POSIX-compliant 
+// https://stackoverflow.com/questions/321849/strptime-equivalent-on-windows
+// https://www.quora.com/Why-doesnt-Microsoft-make-Windows-POSIX-compliant 
