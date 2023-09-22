558019d9c68009106416c9e8891c1827528ca5c1
Ramón Alberto Gómez Urquídez <gomez.ramon2004@gmail.com>
2023-09-22 01:23:34 -0700

    simplified main

--- main.cpp
+++ main.cpp
@@ -1,24 +1,18 @@
 #include "./parse/parse.hpp"
 #include "./sort/mergeSort.hpp"
-#include "./search/sequentialSearch.hpp"
+#include "./search/binarySearch.hpp"
+#include "./print/print.hpp"
 #include <iostream>
 #include <string>
 #include <vector>
 
 int main() {
     try {
+        std::string input = "4TL";
         std::vector<Info> bitacora = parse("bitacora.txt");
         mergeSort(bitacora, 0, bitacora.size() - 1);
-
-       for (auto &&i : bitacora) {
-            std::cout << "dia: " << i.dateTime.tm_mday << " ";
-            std::cout << "mes: " << i.dateTime.tm_mon << " ";
-            std::cout << "año: " << i.dateTime.tm_year << " ";
-            std::cout << "hora: " << i.dateTime.tm_hour << " ";
-            std::cout << "minutos: " << i.dateTime.tm_min << " ";
-            std::cout << "entrada: " << i.enterPoint << " ";
-            std::cout << "ubi: " << i.ubi << "\n";
-        }
+        std::vector<Info> output = binarySearch(input, bitacora);
+        print(output);
 
     } catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
